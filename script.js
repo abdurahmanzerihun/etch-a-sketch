f5a583c0a7fc7a1507d21a699fcbd70bece7e5ee
abdurahman <abdurrahmanzerihun@gmail.com>
2025-09-23 12:31:29 +0300

    Create the logic for 16x16 grid

---
+++ script.js
@@ -0,0 +1,6 @@
+const gridContainer=document.querySelector('.grid-container');
+for(let i=0;i<16*16;i++){
+const square=document.createElement("div");
+square.classList.add("square");
+gridContainer.appendChild(square);
+}
